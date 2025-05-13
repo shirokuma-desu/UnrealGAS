d890c6de803fdd8fae3341b1acc791f608766906
GauTrang <ezorcist.dt@gmail.com>
2025-05-13 15:34:37 +0700

    Create PlayerState

--- Source/Aura/Private/Player/MyPlayerController.cpp
+++ Source/Aura/Private/Player/MyPlayerController.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "PlayerControllers/MyPlayerController.h"
+#include "Player/MyPlayerController.h"
 #include "EnhancedInputComponent.h"
 #include "EnhancedInputSubsystems.h"
 #include "Interfaces/IInteraction.h"
---
+++ Source/Aura/Private/Player/MyPlayerState.cpp
@@ -0,0 +1,9 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Player/MyPlayerState.h"
+
+AMyPlayerState::AMyPlayerState()
+{
+	NetUpdateFrequency = 100.f;
+}
