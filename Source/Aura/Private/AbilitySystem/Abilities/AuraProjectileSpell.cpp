27f0074dcdf26dffe9885810771db80d5ae13ccd
GauTrang <ezorcist.dt@gmail.com>
2026-01-28 15:14:49 +0700

    Create Firebolt GameAbility

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -0,0 +1,15 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AuraProjectileSpell.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Handle,
+                                           const FGameplayAbilityActorInfo* ActorInfo, const FGameplayAbilityActivationInfo ActivationInfo,
+                                           const FGameplayEventData* TriggerEventData)
+{
+	Super::ActivateAbility(Handle, ActorInfo, ActivationInfo, TriggerEventData);
+	UKismetSystemLibrary::PrintString(this, FString("ActivateAbility"),true,true,FLinearColor::Yellow,10.f);
+	
+}
