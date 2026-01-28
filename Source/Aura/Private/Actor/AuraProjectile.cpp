3a7dfbae070584c7ba1e58387b1ba0c86e137658
GauTrang <ezorcist.dt@gmail.com>
2026-01-28 10:09:36 +0700

    Create Firebolt Projectile

---
+++ Source/Aura/Private/Actor/AuraProjectile.cpp
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/AuraProjectile.h"
+
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+
+AAuraProjectile::AAuraProjectile()
+{
+	PrimaryActorTick.bCanEverTick = true;
+	SphereCollision = CreateDefaultSubobject<USphereComponent>("SphereCollision");
+	ProjectileMovementComponent = CreateDefaultSubobject<UProjectileMovementComponent>("ProjectileMovementComponent");
+	
+	SetRootComponent(SphereCollision);
+	SphereCollision->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	SphereCollision->SetCollisionResponseToAllChannels(ECR_Ignore);
+	SphereCollision->SetCollisionResponseToChannel(ECC_WorldDynamic, ECR_Overlap);
+	SphereCollision->SetCollisionResponseToChannel(ECC_WorldStatic, ECR_Overlap);
+	SphereCollision->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	
+	ProjectileMovementComponent->InitialSpeed = 550.f;
+	ProjectileMovementComponent->MaxSpeed = 550.f;
+	ProjectileMovementComponent->ProjectileGravityScale = 0.f;
+}
+
+void AAuraProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	SphereCollision->OnComponentBeginOverlap.AddDynamic(this,&ThisClass::OnSphereOverlap);
+	
+}
+
+void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+	UPrimitiveComponent* OtherComp, int32 Otherbodyindex, bool bfromsweep, const FHitResult& SweepResult)
+{
+	
+}
+
+
---
+++ Source/Aura/Public/Actor/AuraProjectile.h
@@ -0,0 +1,35 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraEffectActor.h"
+#include "AuraProjectile.generated.h"
+
+class UProjectileMovementComponent;
+
+UCLASS()
+class AURA_API AAuraProjectile : public AAuraEffectActor
+{
+	GENERATED_BODY()
+
+public:
+	AAuraProjectile();
+	
+	
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<UProjectileMovementComponent> ProjectileMovementComponent;
+protected:
+	virtual void BeginPlay() override;
+	
+	UFUNCTION()
+	void OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 Otherbodyindex, bool bfromsweep, const FHitResult& SweepResult);
+	
+	
+private:
+	UPROPERTY(VisibleAnywhere)
+	TObjectPtr<USphereComponent> SphereCollision;
+	
+	
+
+};
