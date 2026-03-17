26c3d3a1f42438b617b05c9cfdff683317cd68ab
GauTrang <ezorcist.dt@gmail.com>
2026-03-17 17:06:27 +0700

    Shaman Summon Locations

---
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraSummonAbility.cpp
@@ -0,0 +1,31 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/Abilities/AuraSummonAbility.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+TArray<FVector> UAuraSummonAbility::GetSpawnLocations()
+{
+	const FVector Forward = GetAvatarActorFromActorInfo()->GetActorForwardVector();
+	const FVector Location = GetAvatarActorFromActorInfo()->GetActorLocation();
+	
+	const float DeltaSpread = SpawnSpread / NumberMinionsToSpawn;
+	
+	const FVector LeftOfSpread = Forward.RotateAngleAxis( -SpawnSpread*0.5f, FVector::UpVector);
+	TArray<FVector> SpawnLocations;
+	for (int32 i= 0; i < NumberMinionsToSpawn; i++)
+	{
+		const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread*i, FVector::UpVector);
+		const FVector ChosenSpawnLocation =  Location + Direction * FMath::RandRange(MinSpawnDistance, MaxSpawnDistance);
+		SpawnLocations.Add(ChosenSpawnLocation);
+		
+		
+		DrawDebugSphere(GetWorld(), ChosenSpawnLocation + Direction * MinSpawnDistance, 18.f, 12, FColor::Cyan, false, 3.f);
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(), Location, Location + Direction * MaxSpawnDistance, 4.f, FLinearColor::Green,3.f);
+		DrawDebugSphere(GetWorld(), Location + Direction * MinSpawnDistance, 5.f, 12, FColor::Red, false, 3.f);
+		DrawDebugSphere(GetWorld(), Location + Direction * MaxSpawnDistance, 5.f, 12, FColor::Red, false, 3.f);
+	}
+	
+	return SpawnLocations;
+}
