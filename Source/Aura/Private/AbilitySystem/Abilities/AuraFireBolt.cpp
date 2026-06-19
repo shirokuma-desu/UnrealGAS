bec49b51be46d091cd55549a442a7af794fe4b1a
GauTrang <77010056+shirokuma-desu@users.noreply.github.com>
2026-06-19 20:14:17 +0700

    FireBolt Projectile Spread

--- Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
+++ Source/Aura/Private/AbilitySystem/Abilities/AuraFireBolt.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/Abilities/AuraFireBolt.h"
 
+#include "Kismet/KismetSystemLibrary.h"
+
 FString UAuraFireBolt::GetDescription(int32 Level)
 {
 	
@@ -69,4 +71,47 @@ FString UAuraFireBolt::GetNextLevelDescription(int32 Level)
 			"<Damage>%d</><Default> fire damage with a chance to burn</>"
 			
 			),Level,ManaCost,CoolDown,FMath::Min(Level,NumberOfProjectiles),ScaledDamage);
+}
+
+void UAuraFireBolt::SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag,
+	bool bOverridePitch, float PitchOverride, AActor* HomingTarget)
+{
+	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
+	if (!bIsServer) return;
+	const FVector SocketLocation = ICombatInterface::Execute_GetCombatSocketLocation(GetAvatarActorFromActorInfo(),SocketTag);
+	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+	if (bOverridePitch)
+	{
+		Rotation.Pitch = PitchOverride;
+	}
+	
+	const FVector Forward = Rotation.Vector();
+	const FVector LeftOfSpread = Forward.RotateAngleAxis(-ProjectilesSpread / 2.f, FVector::UpVector);
+	const FVector RightOfSpread = Forward.RotateAngleAxis(ProjectilesSpread /2.f, FVector::UpVector);
+	NumberOfProjectiles = FMath::Min(MaxNumProjectiles,GetAbilityLevel());
+	
+	if (NumberOfProjectiles > 1)
+	{
+		const float DeltaSpread = ProjectilesSpread / (NumberOfProjectiles -1);
+		for (int32 i = 0; i < NumberOfProjectiles; i++)
+		{
+			const FVector Direction = LeftOfSpread.RotateAngleAxis(DeltaSpread * i, FVector::UpVector);
+			const FVector Start = SocketLocation + FVector(0,0,10);
+			UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),
+				Start, Start + Direction * 75.f,1, 
+				FLinearColor::Red, 120, 1);
+		}
+	}
+	else
+	{
+		const FVector Start = SocketLocation + FVector(0,0,5);
+		UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),
+				Start, Start + Forward * 75.f,1, 
+				FLinearColor::Red, 120, 1);
+		//Single Projectile
+	}
+	
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),SocketLocation, SocketLocation + Forward * 100,5, FLinearColor::Green, 120, 2);
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),SocketLocation, LeftOfSpread + Rotation.Vector() * 100,5, FLinearColor::Gray, 120, 1);
+	UKismetSystemLibrary::DrawDebugArrow(GetAvatarActorFromActorInfo(),SocketLocation, RightOfSpread + Rotation.Vector() * 100,5, FLinearColor::Gray, 120, 1);
 }
--- Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraFireBolt.h
@@ -16,4 +16,14 @@ class AURA_API UAuraFireBolt : public UAuraProjectileSpell
 public:
 	virtual FString GetDescription(int32 Level) override;
 	virtual FString GetNextLevelDescription(int32 Level) override;
+	
+	UFUNCTION(BlueprintCallable)
+	void SpawnProjectiles(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, 
+		bool bOverridePitch, float PitchOverride, AActor* HomingTarget);
+protected:
+	
+	UPROPERTY(EditDefaultsOnly, Category= "Firebolt")
+	float ProjectilesSpread = 90.f;
+	UPROPERTY(EditDefaultsOnly, Category= "Firebolt")
+	int32 MaxNumProjectiles = 5;
 };
