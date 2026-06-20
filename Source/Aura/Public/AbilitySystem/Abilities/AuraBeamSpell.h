37153cd1b3b51a947b7ed3224c2ab0a0f65717f9
GauTrang <77010056+shirokuma-desu@users.noreply.github.com>
2026-06-20 14:47:29 +0700

    Aure Beam Spell

---
+++ Source/Aura/Public/AbilitySystem/Abilities/AuraBeamSpell.h
@@ -0,0 +1,36 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AuraDamagedGameplayAbility.h"
+#include "AuraBeamSpell.generated.h"
+
+class AMyPlayerController;
+/**
+ * 
+ */
+UCLASS()
+class AURA_API UAuraBeamSpell : public UAuraDamagedGameplayAbility
+{
+	GENERATED_BODY()
+	
+public:
+	UFUNCTION(BlueprintCallable)
+	void StoreMouseDataInfo(const FHitResult& HitResult);
+	
+	UFUNCTION(BlueprintCallable)
+	void StoreOwnerPlayerController();
+	
+protected:
+	
+	UPROPERTY(BlueprintReadWrite, Category= "Beam")
+	FVector MouseHitLocation;
+	
+	UPROPERTY(BlueprintReadWrite, Category= "Beam")
+	TObjectPtr<AActor> MouseHitActor;
+	
+	UPROPERTY(BlueprintReadWrite, Category= "Beam")
+	TObjectPtr<APlayerController> OwnerPlayerController;
+	
+};
